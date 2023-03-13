50a2b201d8325a9d7f80a8db93fb6465ab9295e7
DmitriyNov <NovokrDm@yandex.ru>
2023-03-13 21:22:07 +0300

    Add GameSaving class

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,12 +1,14 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
     return read().then((response) => json(response))
-      .then((data) => new Promise((resolve, reject) => {
-        const result = JSON.parse(data);
-        resolve(result);
-      }));
+      .then((data) => {
+        const obj = JSON.parse(data);
+        const { id, created, userInfo } = obj;
+        return new GameSaving(id, created, userInfo);
+      });
   }
 }
